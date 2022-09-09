07ce9d1e433654874c5f80c6db4a85aa20c19306
Luvous <thomasvarela2001@outlook.es>
2022-09-09 20:44:18 -0300

    .

--- App.js
+++ App.js
@@ -1,27 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+console.reportErrorsAsExceptions = false;
+import { useState } from 'react';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-import Landing from './components/auth/landing';
+import Landing from './components/auth/Landing';
+import Register from './components/auth/Register'
 
 export default function App() {
-  const Stack = createStackNavigator();
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Landing'>
-        <Stack.Screen name='Landing' component={Landing} options={{ headerShown:false }} />
-      </Stack.Navigator>
+  const Stack = createNativeStackNavigator();
 
-    </NavigationContainer>
-  );
+  const firebaseConfig = {
+    apiKey: "AIzaSyAoFv7z5RHySg0-c0sseg0XME9cYtvkO6o",
+    authDomain: "exptagram.firebaseapp.com",
+    projectId: "exptagram",
+    storageBucket: "exptagram.appspot.com",
+    messagingSenderId: "969046117555",
+    appId: "1:969046117555:web:1cf2d8e33079080b0a34c6",
+    measurementId: "G-L9JH11YHC9"
+  };
+  const app = initializeApp(firebaseConfig);
+
+  const auth = getAuth(app)
+
+  const [loaded, isLoaded] = useState(false)
+  const [loggedIn, isLogged] = useState(false)
+
+
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      isLogged(false)
+      isLoaded(false)
+    }
+    else {
+      isLogged(false)
+      isLoaded(true)
+    }
+  })
+
+  if (!loaded) {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center'
+      }}>
+        <Text>
+          Loading...
+        </Text>
+      </View>
+    )
+  }
+  else return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Landing'>
+          <Stack.Screen name='Landing' component={Landing} options={{ headerShown: false }} />
+          <Stack.Screen name='Register' component={Register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import { View, Button, TextInput } from 'react-native';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-export default function Register() {
+export default function Login() {
     const auth = getAuth();
 
-    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const onSignUp = () => {
-        createUserWithEmailAndPassword(auth, email, password)
+        signInWithEmailAndPassword(auth, email, password)
         .then((result)=>{
             console.log(result);
         })
@@ -22,10 +21,6 @@ export default function Register() {
     return (
         <View>
             <TextInput
-                placeholder='name'
-                onChangeText={(name) => setName(name) }
-            />
-            <TextInput
                 placeholder='email'
                 onChangeText={(email)  => setEmail(email) }
             />
@@ -36,7 +31,7 @@ export default function Register() {
             />
             <Button 
                 onPress={()=>onSignUp()}
-                title='Sign Up'
+                title='Sign In'
             />
         </View>
     )
